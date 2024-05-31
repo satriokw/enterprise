24119cb47d95760ebd0fd68ac6ec2b3eed85dc9c
satrio.bayu <satrio.bayu@koinworks.com>
2024-05-31 14:32:10 +0700

    chore cleanup

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
 // console.log("welcome to USS enterprise")
 const data = require("./flow-1.json")
-
-const TYPE_CONSTANT = {
-  "text": "string",
-  "number": "number",
-  "password": "string",
-}
-
 // console.log(data)
 
-// let rowId = data.rows.map((item) => item.cols[0].id)
 let rowType = data.rows.map((item) => item.cols[0].type)
 let rowLabel = data.rows.map((item) => item.cols[0].label)
 let rowValue = data.rows.map((item) => item.cols[0].value)
-
-let refinedValue = rowValue.map(item => item.replace('{{', '').replace('}}', ''));
-// console.log(rowId)
-// console.log(rowLabel)
-// console.log(rowType)
-// console.log(rowValue)
-// console.log(refinedValue)
-
-let type = rowType.map(item => TYPE_CONSTANT[item])
-
-const formData = {
-  type: "",
-  title: "",
-}
-
-// refinedValue.forEach(item => {
-//   resData.properties[item] = {};
-// });
-
-// console.log(resData);
-
 
 const transformedData = {
   "title": data.metadata.name,
@@ -45,6 +16,7 @@ const transformedData = {
 
 const fields = {};
 
+// main function
 data.rows.forEach(row => {
   const key = row.cols[0].value.replace('{{', '').replace('}}', '');
   const colData = row.cols[0];
