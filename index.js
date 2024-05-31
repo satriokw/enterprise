acaec1c854373d8aee2c4632b0acb85fdf68ec5d
satrio.bayu <satrio.bayu@koinworks.com>
2024-05-31 12:26:42 +0700

    commit lagi

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("welcome to USS enterprise")
+// console.log("welcome to USS enterprise")
+// import data from "./flow-1.json"
+const data = require("./flow-1.json")
+
+// console.log(data)
+
+let row = data.rows.map((item) => item.cols)
+
+console.log(row)
+// console.log(Array.isArray(data.rows))
