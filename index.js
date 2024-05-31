6ce2f9d31ef3f54606c352449db3a293a1c579eb
satrio.bayu <satrio.bayu@koinworks.com>
2024-05-31 12:51:28 +0700

    pushhh

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const data = require("./flow-1.json")
 
 // console.log(data)
 
-let row = data.rows.map((item) => item.cols)
+let rowId = data.rows.map((item) => item.cols[0].id)
+let rowLabel = data.rows.map((item) => item.cols[0].label)
+let rowValue = data.rows.map((item) => item.cols[0].value)
 
-console.log(row)
+let refinedValue = rowValue.map(item => item.replace('{{', '').replace('}}', ''));
+console.log(rowId)
+console.log(rowLabel)
+console.log(rowValue)
+console.log(refinedValue)
 // console.log(Array.isArray(data.rows))
